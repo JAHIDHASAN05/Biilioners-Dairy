0900243cde3b41c1c723f39d4b7328fb369bdfb2
JAHID HASAN <developerjahid@gmail.com>
2023-10-28 16:48:09 +0600

    set up commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const loadBillioner= async()=>{
+//     const url =`https://forbes400.onrender.com/api/forbes400/`
+//     const res = await fetch(url)
+//     const data=await res.json()
+//     console.log(data[0])
+// }
+
+
+// loadBillioner()
